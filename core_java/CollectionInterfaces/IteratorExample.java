2160d26293c6aee734c44210dfc24144b3dccd39
kalyanappari <asaikalyan2005@gmail.com>
2026-01-15 11:06:37 +0530

    Worked on Iterator

---
+++ core_java/CollectionInterfaces/IteratorExample.java
@@ -0,0 +1,36 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Iterable;
+
+public class IteratorExample {
+    
+    public static void main(String[] args) {
+        
+        Collection<Integer> obj = new ArrayList<>();
+
+        obj.add(1);
+        obj.add(2);
+        obj.add(3);
+        obj.add(4);
+
+        Iterator<Integer> it = obj.iterator();
+
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+        //Executing the remove method.
+
+        Iterator<Integer> it2 = obj.iterator();
+
+        while (it2.hasNext()) {
+
+            if(it2.next()%2 == 0){
+                it2.remove();
+            }
+        }
+
+        System.out.println(obj);
+    }
+}
